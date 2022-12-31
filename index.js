d48d0e907075d0fb1cebc343b494ea7de4ab8db5
AnthonyPowell <anthonydewpow@gmail.com>
2022-12-31 15:31:58 -0600

    commit tree afd037275ad4ee2ff82bb74f2fb0a59ce781664c

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+main.remove();
+
+const newHeader = document.createElement("H1");
+newHeader.setAttribute('id','victory');
+newHeader.textContent = 'Ricky Bobby is the champion';
